69cdda1e4a4e7e9daa105682a091b985478e9408
TraviFravi <travi1500@gmail.com>
2020-10-06 02:08:05 -0400

    added HTML, CSS, and JS files

---
+++ script.js
@@ -0,0 +1,23 @@
+// When You click the Start Button the quiz starts
+
+
+
+
+// Timer starts
+
+if () {
+
+} else () {
+
+};
+
+// Presented with a question
+// prompt
+
+// answer the question and presented with another question
+
+// if i answer the question incorrectly the time is subtracted
+
+//When all questions are answered  or the timer reaches 0, the game is over.
+
+//When the game is over then i can save my initals and score
